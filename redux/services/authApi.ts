a7ade7164c1a80624ccf2c1519a78ed0578c2451
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-27 20:14:13 +0100

    refactor: :recycle: move the api url into the .env.local file

--- redux/services/authApi.ts
+++ redux/services/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
   }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
