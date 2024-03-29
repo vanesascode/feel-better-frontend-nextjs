4936691e34f0c46aa9fa4780cdd99451ea566f8a
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-29 18:41:04 +0100

    refactor: :recycle: modify the tailwind classes and turn it into an arrow function component
    also add the ScrollToTop component

--- components/commons/contentFrame.tsx
+++ components/commons/contentFrame.tsx
@@ -1,17 +1,19 @@
 import { ReactNode } from "react";
+import ScrollToTop from "./scrollToTop";
 
 type ContentFrameProps = {
   children: ReactNode;
 };
 
-function ContentFrame({ children }: ContentFrameProps) {
+const ContentFrame = ({ children }: ContentFrameProps) => {
   return (
-    <div className="bg-gray-gradient flex justify-center items-center px-10 pt-10 pb-20">
-      <div className="bg-dark flex justify-center items-center p-10 w-full rounded-lg max-w-[1400px]">
+    <div className="bg-gray-gradient flex justify-center items-center px-10 pt-10 pb-20 relative">
+      <ScrollToTop />
+      <div className="bg-dark flex justify-center items-center p-10 w-full rounded-xl max-w-[1400px]">
         {children}
       </div>
     </div>
   );
-}
+};
 
 export default ContentFrame;
