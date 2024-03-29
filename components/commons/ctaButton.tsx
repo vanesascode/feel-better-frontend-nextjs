7cdfae906ddcbbe87c5a5698c13cdbd9393e5760
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-29 18:44:47 +0100

    refactor: :recycle: modify the tailwind classes and the props of the ctaButton component
    also turn it into an arrow function component

--- components/commons/ctaButton.tsx
+++ components/commons/ctaButton.tsx
@@ -2,15 +2,24 @@ import { ReactNode } from "react";
 
 type CtaButtonProps = {
   children: ReactNode;
-  disabled: boolean;
+  disabled?: boolean;
   type: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
+  darkerShadow?: boolean;
 };
 
-function CtaButton({ children, disabled, type, onClick }: CtaButtonProps) {
+const CtaButton = ({
+  children,
+  disabled,
+  type,
+  onClick,
+  darkerShadow,
+}: CtaButtonProps) => {
   return (
     <button
-      className="py-2 rounded-full bg-cta-green text-heading3-bold w-[250px] hover:bg-cta-green-hover"
+      className={`py-2 rounded-full bg-cta-green md:text-heading3-bold text-body-bold md:w-[250px] w-[180px] hover:bg-cta-green-hover text-white shadow-md ${
+        darkerShadow ? "shadow-black" : "shadow-shadow-green"
+      }  transition duration-500 ease-in-out  `}
       disabled={disabled}
       type={type}
       onClick={onClick}
@@ -18,6 +27,6 @@ function CtaButton({ children, disabled, type, onClick }: CtaButtonProps) {
       {children}
     </button>
   );
-}
+};
 
 export default CtaButton;
