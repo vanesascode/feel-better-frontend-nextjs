7e1afab596ee51336b9f550aa7bd313e6127f152
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-30 17:23:41 +0100

    refactor: :recycle: turn the type into an interface for consistency in the contentFrame component
    also add a tailwind class

--- components/commons/contentFrame.tsx
+++ components/commons/contentFrame.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
 import ScrollToTop from "./scrollToTop";
 
-type ContentFrameProps = {
+interface ContentFrameProps {
   children: ReactNode;
-};
+}
 
 const ContentFrame = ({ children }: ContentFrameProps) => {
   return (
     <div className="bg-gray-gradient flex justify-center items-center px-10 pt-10 pb-20 relative">
       <ScrollToTop />
-      <div className="bg-dark flex justify-center items-center p-10 w-full rounded-xl max-w-[1400px]">
+      <div className="bg-dark flex justify-center flex-col items-center p-10 w-full rounded-xl max-w-[1400px]">
         {children}
       </div>
     </div>
