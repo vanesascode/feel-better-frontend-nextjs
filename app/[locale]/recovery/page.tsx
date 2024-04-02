de7fed52e6da8265f49b8db85bbfed0a5b2f4565
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-02 14:09:01 +0200

    feat: :sparkles: add the recovery page for when user has forgotten password

---
+++ app/[locale]/recovery/page.tsx
@@ -0,0 +1,47 @@
+import initTranslations from "@/app/i18n";
+import TranslationsProvider from "@/components/TranslationProvider";
+import ContentFrame from "@/components/commons/contentFrame";
+import Header from "@/components/commons/header";
+import TokenMaintainer from "@/components/commons/tokenMaintainer";
+import Navbar from "@/components/navbar/navbar";
+import RecoveryForm from "@/components/recovery/recoveryForm";
+
+const i18nNamespaces = ["recovery", "navbar"];
+
+export default async function Account({
+  params,
+}: {
+  params: { locale: string };
+}) {
+  const { t, resources } = await initTranslations(
+    params.locale,
+    i18nNamespaces,
+    null,
+    null
+  );
+
+  return (
+    <div>
+      <TokenMaintainer />
+      <TranslationsProvider
+        resources={resources}
+        locale={params.locale}
+        namespaces={i18nNamespaces}
+      >
+        <header>
+          <Navbar />
+          <Header>
+            <h1 className="text-heading2-bold text-center px-3">
+              {t("forgot-password")}
+            </h1>
+          </Header>
+        </header>
+        <main>
+          <ContentFrame>
+            <RecoveryForm />
+          </ContentFrame>
+        </main>
+      </TranslationsProvider>
+    </div>
+  );
+}
