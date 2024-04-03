f8feb6f3b5906da3fec7f46108294664e6888609
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-03 22:14:16 +0200

    feat: :sparkles: add the resetPassword function in the users api file

--- api/users/users.ts
+++ api/users/users.ts
@@ -5,6 +5,28 @@ export async function fetchExistingUsersEmails() {
     const emails = data.map((user: any) => user.email);
     return emails;
   } catch (error) {
-    console.error("Error fetching existing users' emails:", error);
+    return Error("Error fetching existing users emails");
+  }
+}
+
+interface ResetPasswordProps {
+  email: string;
+  password: string;
+}
+
+export async function resetPassword({ email, password }: ResetPasswordProps) {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/users", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.status !== 200) {
+      throw new Error("Error resetting password");
+    }
+  } catch (error) {
+    throw new Error("Error resetting password");
   }
 }
