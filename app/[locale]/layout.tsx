7a336bfa19de9b701a425115f1a549080e813355
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-03 22:15:14 +0200

    feat: :sparkles: add the code to connect the app with google analytics in the main layout file

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/redux/provider";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JD5GLKZHEE"
+        ></Script>
+        <Script id="google-analytics">
+          {` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JD5GLKZHEE');`}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Providers>{children} </Providers>
       </body>
