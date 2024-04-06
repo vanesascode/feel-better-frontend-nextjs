5fc9f2fa5b1f0e405da4da60570b76eb1cce53a2
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-06 19:46:32 +0200

    refactor: :recycle: add the activityCard component into the homeActivites one for refactoring

--- components/home/homeActivities.tsx
+++ components/home/homeActivities.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useTranslation } from "react-i18next";
-import Image from "next/image";
-import Link from "next/link";
+import ActivityCard from "./activityCard";
 
 const HomeActivities = () => {
   const { t } = useTranslation();
@@ -14,67 +13,31 @@ const HomeActivities = () => {
           <h3 className="md:text-heading2-bold text-heading3-bold text-white text-center md:pt-20 sm:pt-14 pt-10 md:pb-14 sm:pb-10 pb-8 p-horizontal">
             {t("activities-title")}
           </h3>
-          <div className="flex sm2:flex-row flex-col justify-around sm2:items-start items-center md:pb-20 sm:pb-14 pb-10">
-            <article>
-              <Link href="/elegir-pensamiento">
-                <div className="flex flex-col justify-center items-center">
-                  <div className="flex justify-center items-center bg-dark-green-2 px-16 py-10 sm2:mr-5 sm:mr-0 rounded-3xl">
-                    <Image
-                      src="/home/head-icon.svg"
-                      alt="activities"
-                      width={70}
-                      height={70}
-                      className=""
-                    />
-                  </div>
-                  <div className="max-w-32 flex justify-center items-center pt-5 md:pb-0 pb-5">
-                    <p className="text-center text-white md:text-body-regular text-base-thin font-source ">
-                      {t("activity-one")}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            </article>
-            <article>
-              <Link href="/meditaciones">
-                <div className="flex flex-col justify-center items-center">
-                  <div className="flex justify-center items-center bg-dark-green-1 px-16 py-[47px] rounded-3xl">
-                    <Image
-                      src="/home/loto-icon.svg"
-                      alt="activities"
-                      width={70}
-                      height={70}
-                      className=""
-                    />
-                  </div>
-                  <div className="max-w-32 flex justify-center items-center pt-5 md:pb-0 pb-5">
-                    <p className="text-center text-white md:text-body-regular text-base-thin font-source ">
-                      {t("activity-two")}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            </article>
-            <article>
-              <Link href="/aprender">
-                <div className="flex flex-col justify-center items-center">
-                  <div className="flex justify-center items-center bg-light-green px-16 py-10 rounded-3xl sm2:ml-5 sm:ml-0">
-                    <Image
-                      src="/home/learn-icon.svg"
-                      alt="activities"
-                      width={70}
-                      height={70}
-                      className=""
-                    />
-                  </div>
-                  <div className="max-w-32 flex justify-center items-center pt-5 ">
-                    <p className="text-center text-white md:text-body-regular text-base-thin font-source ">
-                      {t("activity-three")}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            </article>
+          <div className="flex sm2:flex-row flex-col justify-around sm2:items-start items-center md:pb-20 sm:pb-14 pb-10 gap-3">
+            <ActivityCard
+              link="/elegir-pensamiento"
+              image="/home/head-icon.svg"
+              alt="head icon"
+              greenDark
+            >
+              {t("activity-one")}
+            </ActivityCard>
+            <ActivityCard
+              link="/meditaciones"
+              image="/home/loto-icon.svg"
+              alt="meditation icon"
+              greenMedium
+            >
+              {t("activity-two")}
+            </ActivityCard>
+            <ActivityCard
+              link="/aprender"
+              image="/home/learn-icon.svg"
+              alt="learn icon"
+              greenLight
+            >
+              {t("activity-two")}
+            </ActivityCard>
           </div>
         </div>
       </section>
