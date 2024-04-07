fea67d43c501a9910143f8e2fc0b6427b5d375ab
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-07 18:12:31 +0200

    refactor: :recycle: add the green boolean prop to the ctaButton component in the homeExplanationActivities component
    also move the contentFrame component into the main home page for consistency

--- components/home/homeExplanationActivities.tsx
+++ components/home/homeExplanationActivities.tsx
@@ -2,7 +2,6 @@
 
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
-import ContentFrame from "../commons/contentFrame";
 import CtaButton from "../commons/ctaButton";
 import TextToSpeech from "../commons/textToSpeech";
 import ActivityImage from "./activityImage";
@@ -12,116 +11,114 @@ const HomeExplanationActivities = () => {
 
   return (
     <section>
-      <ContentFrame>
-        <div className="text-white">
-          <TextToSpeech
-            textToSpeak={
-              t("activity-one") +
-              t("activity-one-explanation") +
-              t("activity-two") +
-              t("activity-two-explanation") +
-              t("activity-three") +
-              t("activity-three-explanation")
-            }
-          />
-          <article>
-            <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
-              <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
-                <h4 className="md:text-heading2-bold text-heading3-bold">
-                  {t("activity-one")}
-                </h4>
-                <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
-                  {" "}
-                  {t("activity-one-explanation")}
-                </p>
-                <Link href="/pensamientos-negativos">
-                  <CtaButton type="button" darkerShadow>
-                    {t("learn-more")}
-                  </CtaButton>
-                </Link>
-              </div>
-              <div className="lg:w-[48%] hidden lg:block max-sm2:block">
-                <ActivityImage
-                  image="/thoughts/thoughts-cover-background.jpg"
-                  alt="thoughts activity"
-                />
-              </div>
+      <div className="text-white">
+        <TextToSpeech
+          textToSpeak={
+            t("activity-one") +
+            t("activity-one-explanation") +
+            t("activity-two") +
+            t("activity-two-explanation") +
+            t("activity-three") +
+            t("activity-three-explanation")
+          }
+        />
+        <article>
+          <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
+            <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
+              <h4 className="md:text-heading2-bold text-heading3-bold">
+                {t("activity-one")}
+              </h4>
+              <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
+                {" "}
+                {t("activity-one-explanation")}
+              </p>
+              <Link href="/pensamientos-negativos">
+                <CtaButton type="button" darkerShadow green>
+                  {t("learn-more")}
+                </CtaButton>
+              </Link>
             </div>
-            <div className="block lg:hidden max-sm2:hidden pb-10">
+            <div className="lg:w-[48%] hidden lg:block max-sm2:block">
               <ActivityImage
-                image="/home/thoughts-cover-narrow.jpg"
+                image="/thoughts/thoughts-cover-background.jpg"
                 alt="thoughts activity"
               />
             </div>
-          </article>
-          <article>
-            <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
-              <div className="lg:w-[48%] hidden lg:block">
-                <ActivityImage
-                  image="/meditation/meditation-cover-background.jpg"
-                  alt="meditation activity"
-                />
-              </div>
-              <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
-                <h4 className="md:text-heading2-bold text-heading3-bold">
-                  {t("activity-two")}
-                </h4>
-                <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
-                  {" "}
-                  {t("activity-two-explanation")}
-                </p>
-                <Link href="/meditacion">
-                  <CtaButton type="button" darkerShadow>
-                    {t("learn-more")}
-                  </CtaButton>
-                </Link>
-              </div>
-              <div className="lg:w-[48%] hidden max-sm2:block">
-                <ActivityImage
-                  image="/meditation/meditation-cover-background.jpg"
-                  alt="meditation activity"
-                />
-              </div>
-            </div>
-            <div className="block lg:hidden max-sm2:hidden pb-10">
+          </div>
+          <div className="block lg:hidden max-sm2:hidden pb-10">
+            <ActivityImage
+              image="/home/thoughts-cover-narrow.jpg"
+              alt="thoughts activity"
+            />
+          </div>
+        </article>
+        <article>
+          <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
+            <div className="lg:w-[48%] hidden lg:block">
               <ActivityImage
-                image="/home/meditation-cover-narrow.jpg"
+                image="/meditation/meditation-cover-background.jpg"
                 alt="meditation activity"
               />
             </div>
-          </article>
-          <article>
-            <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
-              <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
-                <h4 className="md:text-heading2-bold text-heading3-bold">
-                  {t("activity-three")}
-                </h4>
-                <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
-                  {" "}
-                  {t("activity-three-explanation")}
-                </p>
-                <Link href="/aprender">
-                  <CtaButton type="button" darkerShadow>
-                    {t("learn-more")}
-                  </CtaButton>
-                </Link>
-              </div>
-              <div className="lg:w-[48%] hidden lg:block max-sm2:block">
-                <ActivityImage
-                  image="/learn/learn-cover-background.jpg"
-                  alt="learn activity"
-                />
-              </div>
+            <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
+              <h4 className="md:text-heading2-bold text-heading3-bold">
+                {t("activity-two")}
+              </h4>
+              <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
+                {" "}
+                {t("activity-two-explanation")}
+              </p>
+              <Link href="/meditacion">
+                <CtaButton type="button" darkerShadow green>
+                  {t("learn-more")}
+                </CtaButton>
+              </Link>
             </div>
-            <div className="block lg:hidden max-sm2:hidden pb-10">
+            <div className="lg:w-[48%] hidden max-sm2:block">
               <ActivityImage
-                image="/home/learn-cover-narrow.jpg"
+                image="/meditation/meditation-cover-background.jpg"
+                alt="meditation activity"
+              />
+            </div>
+          </div>
+          <div className="block lg:hidden max-sm2:hidden pb-10">
+            <ActivityImage
+              image="/home/meditation-cover-narrow.jpg"
+              alt="meditation activity"
+            />
+          </div>
+        </article>
+        <article>
+          <div className="md:flex-row flex-col flex justify-between pb-10 items-center">
+            <div className="lg:w-[48%] w-full max-sm2:pb-10 ">
+              <h4 className="md:text-heading2-bold text-heading3-bold">
+                {t("activity-three")}
+              </h4>
+              <p className="md:text-body-thin text-base-thin pb-10 pt-5 text-gray">
+                {" "}
+                {t("activity-three-explanation")}
+              </p>
+              <Link href="/aprender">
+                <CtaButton type="button" darkerShadow green>
+                  {t("learn-more")}
+                </CtaButton>
+              </Link>
+            </div>
+            <div className="lg:w-[48%] hidden lg:block max-sm2:block">
+              <ActivityImage
+                image="/learn/learn-cover-background.jpg"
                 alt="learn activity"
               />
             </div>
-          </article>
-        </div>
-      </ContentFrame>
+          </div>
+          <div className="block lg:hidden max-sm2:hidden pb-10">
+            <ActivityImage
+              image="/home/learn-cover-narrow.jpg"
+              alt="learn activity"
+            />
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
