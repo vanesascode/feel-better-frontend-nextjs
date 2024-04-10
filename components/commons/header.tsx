3fd739193f3bf1ebc41ec347e8d610784249bb2d
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-10 20:02:53 +0200

    refactor: :recycle: remove an unnecessary div in the header commons component

--- components/commons/header.tsx
+++ components/commons/header.tsx
@@ -6,8 +6,8 @@ interface HeaderProps {
 
 const Header = ({ children }: HeaderProps) => {
   return (
-    <div className="md:h-72 h-56 bg-dark flex justify-center items-center">
-      <div className="pt-20 text-white">{children}</div>
+    <div className="md:h-72 h-56 bg-dark flex flex-col justify-center items-center pt-20 text-white">
+      {children}
     </div>
   );
 };
