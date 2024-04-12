14fbc7e9cff998dc0fe61c7cb15060e11153a644
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-12 19:14:36 +0200

    feat: :sparkles: add the useLanguageChanger custom hook

---
+++ hooks/useLanguageChanger.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useTranslation } from "react-i18next";
+import i18nConfig from "@/i18nConfig";
+
+export const useLanguageChanger = () => {
+  const { i18n } = useTranslation();
+  const currentLocale = i18n.language;
+  const router = useRouter();
+  const currentPathname = usePathname();
+  const [languageValue, setLanguageValue] = useState(currentLocale);
+
+  useEffect(() => {
+    const changeLanguageValue = () => {
+      const newLocale = languageValue;
+      const days = 1;
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      const expires = date.toUTCString();
+      document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
+      if (currentLocale === i18nConfig.defaultLocale) {
+        router.push("/" + newLocale + currentPathname);
+      } else {
+        router.push(
+          currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
+        );
+      }
+      router.refresh();
+    };
+    changeLanguageValue();
+  }, [languageValue]);
+
+  return { setLanguageValue, currentLocale };
+};
