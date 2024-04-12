fdfd1c1ccb1dbb1798e96ab8ae3afbad1859c0a3
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-12 19:22:44 +0200

    refactor: :recycle: mix the negativeThoughtChosen and the positiveThoughtChosen components into the thoughtChosen one for reusability

---
+++ components/commons/thoughtChosen.tsx
@@ -0,0 +1,14 @@
+import { formatText } from "@/utils/formattingUtils";
+
+const ThoughtChosen = ({ thought }: { thought: string | null | undefined }) => {
+  const formattedThought = formatText(thought || "");
+
+  return (
+    <div className="flex justify-center items-center md:text-body-thin text-base-thin text-gray bg-dark-green-2 lg:px-16 md:px-10 px-5 py-5 rounded-lg gap-5 xs:flex-row flex-col max-w-[1024px]">
+      <p className="block xxs:hidden">{formattedThought}</p>
+      <p className="hidden xxs:block">{thought}</p>
+    </div>
+  );
+};
+
+export default ThoughtChosen;
