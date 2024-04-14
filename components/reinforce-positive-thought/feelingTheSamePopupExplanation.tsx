566efddb67fdd4fa53bdc7ea069a62acb00244f4
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-14 18:51:43 +0200

    feat: :sparkles: add the UI and copies to the FeelingTheSamePopupExplanation component

--- components/reinforce-positive-thought/feelingTheSamePopupExplanation.tsx
+++ components/reinforce-positive-thought/feelingTheSamePopupExplanation.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { motion, AnimatePresence } from "framer-motion";
 import CtaButton from "../commons/ctaButton";
+import PopupsBackground from "../commons/popupsBackground";
 
 interface FeelingTheSamePopupExplanationProps {
   saveThoughts: () => void;
@@ -29,13 +30,7 @@ const FeelingTheSamePopupExplanation = ({
       <AnimatePresence>
         {showFeelingTheSamePopupExplanation && (
           <>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.35, ease: "easeInOut" }}
-              exit={{ opacity: 0 }}
-              className="fixed top-0 left-0 right-0 w-full h-full bg-[#ffffff] bg-opacity-30 z-10"
-            ></motion.div>
+            <PopupsBackground />
             <motion.div
               initial={{ opacity: 0 }}
               animate={{
@@ -43,15 +38,32 @@ const FeelingTheSamePopupExplanation = ({
               }}
               transition={{ duration: 0.35, ease: "easeInOut" }}
               exit={{ opacity: 0 }}
-              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[250px] md:w-[420px] xs:w-[300px] bg-dark p-10  rounded-lg"
+              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[290px] md:w-[720px] sm2:w-[540px] sm:w-[400px] xs:w-[360px] bg-dark p-10 rounded-lg max-h-[90vh] overflow-y-auto"
             >
-              <div className="flex flex-col items-center">
-                <h2 className="text-heading3-bold text-center text-white">
-                  {t("your-details-correctly-saved")}
+              <div className="flex flex-col items-center text-white gap-10 ">
+                <h2 className="md:text-heading1-bold text-heading2-bold text-center">
+                  {t("dont-worry")}
                 </h2>
-                <p className="text-body-thin text-center text-white my-5">
-                  {t("login-again")}
-                </p>
+                <div className="flex flex-col justify-start gap-5">
+                  <p className="md:text-body-thin text-base-thin text-gray">
+                    {t("first-paragraph")}{" "}
+                    <span className="md:text-body-bold text-white text-base-bold">
+                      {t("normal")}
+                    </span>
+                  </p>
+                  <p className="md:text-body-thin text-base-thin text-gray">
+                    {t("second-paragraph")}
+                  </p>
+                  <p className="md:text-body-thin text-base-thin text-gray">
+                    {t("third-paragraph")}
+                  </p>
+                  <p className="md:text-body-thin text-base-thin text-gray">
+                    {t("fourth-paragraph")}{" "}
+                    <span className="md:text-body-bold text-white text-base-bold">
+                      {t("patience")}
+                    </span>
+                  </p>
+                </div>
                 {savingThoughtsError && (
                   <p className="text-red-500 text-base-regular text-center mt-5">
                     {t("server-error")}
@@ -63,7 +75,7 @@ const FeelingTheSamePopupExplanation = ({
                   darkerShadow
                   green
                 >
-                  {t("finalizar")}
+                  {t("finish")}
                 </CtaButton>
               </div>
             </motion.div>
