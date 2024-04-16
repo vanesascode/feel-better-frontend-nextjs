ca6f24f6d55261cc9f123b80361236869c26f8e8
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-16 20:48:22 +0200

    feat: :sparkles: add error validation UI to the FollowUpContent component

--- components/follow-up/followUpContent.tsx
+++ components/follow-up/followUpContent.tsx
@@ -3,23 +3,30 @@
 import CalendarUi from "./calendarUi";
 import { useGetThoughtsByUser } from "@/hooks/useGetThoughtsByUser";
 import { useGetThoughtsByUserAndFeeling } from "@/hooks/useGetThoughtsByUserAndFeeling";
-import FeelingFilterOfThoughts from "./feelingFilterOfThoughts";
+import { useTranslation } from "react-i18next";
 
 const FollowUpContent = () => {
-  const { thoughts, token } = useGetThoughtsByUser();
-
-  const { thoughtsByFeeling, fetchThoughtsByFeeling } =
-    useGetThoughtsByUserAndFeeling();
+  const { t } = useTranslation();
+  const { thoughts, token, getThouthgsByUserError } = useGetThoughtsByUser();
+  const {
+    thoughtsByFeeling,
+    fetchThoughtsByFeeling,
+    getThoughtsByUserAndFeelingError,
+  } = useGetThoughtsByUserAndFeeling();
 
   return (
     <>
-      <FeelingFilterOfThoughts
-        fetchThoughtsByFeeling={fetchThoughtsByFeeling}
-      />
+      {(getThouthgsByUserError && (
+        <p className="text-red-400">{t("sorry-server-error")}</p>
+      )) ||
+        (getThoughtsByUserAndFeelingError && (
+          <p className="text-red-400">{t("sorry-server-error")}</p>
+        ))}
       <CalendarUi
         thoughts={thoughts}
         token={token}
         thoughtsByFeeling={thoughtsByFeeling}
+        fetchThoughtsByFeeling={fetchThoughtsByFeeling}
       />
     </>
   );
