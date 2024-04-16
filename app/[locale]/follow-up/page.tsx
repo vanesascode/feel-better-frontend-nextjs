d17bcf5a950ef60e4e055e99d35853e4933e8585
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-16 20:28:20 +0200

    refactor: :recycle: move the use client content from the follow-up page to the followUpUseClientPage component

--- app/[locale]/follow-up/page.tsx
+++ app/[locale]/follow-up/page.tsx
@@ -1,19 +1,16 @@
-import Navbar from "@/components/navbar/navbar";
 import initTranslations from "@/app/i18n";
 import TranslationsProvider from "@/components/TranslationProvider";
 import TokenMaintainer from "@/components/commons/tokenMaintainer";
-import ContentFrame from "@/components/commons/contentFrame";
-import FollowUpContent from "@/components/follow-up/followUpContent";
-import Header from "@/components/commons/header";
+import FollowUpUseClientPage from "@/components/follow-up/followUpUseClientPage";
 
-const i18nNamespaces = ["home", "navbar"];
+const i18nNamespaces = ["follow-up", "navbar"];
 
 export default async function FollowUp({
   params,
 }: {
   params: { locale: string };
 }) {
-  const { t, resources } = await initTranslations(
+  const { resources } = await initTranslations(
     params.locale,
     i18nNamespaces,
     null,
@@ -28,20 +25,7 @@ export default async function FollowUp({
         locale={params.locale}
         namespaces={i18nNamespaces}
       >
-        <header>
-          <Navbar />
-          <Header>
-            <h1 className="md:text-heading2-bold text-heading3-bold text-center px-3 mb-6">
-              {t("is-thought-true")}
-            </h1>
-            <div className="max-w-[400px] sm:mb-0 mb-3">sdfsdf</div>
-          </Header>
-        </header>
-        <main>
-          <ContentFrame>
-            <FollowUpContent />
-          </ContentFrame>
-        </main>
+        <FollowUpUseClientPage />
       </TranslationsProvider>
     </>
   );
