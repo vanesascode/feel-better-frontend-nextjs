a2c101e6393a968ca2f99764e6a4808884f50c23
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-18 20:33:38 +0200

    refactor: :recycle: modify the CSS of the languageChangerHorizontalNavbar component for responsiveness and accessibility

--- components/navbar/languageChangerHorizontalNavbar.tsx
+++ components/navbar/languageChangerHorizontalNavbar.tsx
@@ -9,9 +9,9 @@ const LanguageChangerHorizontalNavbar = () => {
   return (
     <>
       <div className="relative py-3 hidden lg:block">
-        <div className="flex items-center gap-2 bg-dark border-[1px] border-gray p-5 py-2 rounded-full hover:bg-black outline-none active:outline-none w-300px uppercase transition duration-500 ease-in-out cursor-pointer">
+        <div className="flex items-center gap-2 bg-dark border-[1px] border-gray p-5 py-2 rounded-full hover:bg-black outline-none active:outline-none uppercase transition duration-500 ease-in-out cursor-pointer">
           <div>{currentLocale}</div>
-          <ChevronDownIcon className="h-5 w-5 mb-[-3px]" />
+          <ChevronDownIcon className="h-5 w-5 mb-[-0.1875rem]" />
         </div>
         <div className="navbar-language-submenu">
           <button
