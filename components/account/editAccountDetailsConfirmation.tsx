96b7c981c34cbd2a6655ac6435c2bb68dc7207cc
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-18 20:25:42 +0200

    refactor: :recycle: modify the CSS of the editAccountDetailsConfirmation component for responsiveness and accessibility

--- components/account/editAccountDetailsConfirmation.tsx
+++ components/account/editAccountDetailsConfirmation.tsx
@@ -36,7 +36,7 @@ const EditAccountDetailsConfirmation = ({
               }}
               transition={{ duration: 0.35, ease: "easeInOut" }}
               exit={{ opacity: 0 }}
-              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[250px] md:w-[420px] xs:w-[300px] bg-dark p-10  rounded-lg"
+              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[15.62rem] md:w-[26.25rem] xs:w-[18.75rem] bg-dark p-10 rounded-lg"
             >
               <div className="flex flex-col items-center">
                 <h2 className="text-heading3-bold text-center text-white">
