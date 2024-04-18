41db6f7aa16454679bbfe5f01a103c35a7114993
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-18 20:21:44 +0200

    refactor: :recycle: modify the CSS of the isNegativeThoughtTrue page for responsiveness and accessibility

--- app/[locale]/is-negative-thought-true/page.tsx
+++ app/[locale]/is-negative-thought-true/page.tsx
@@ -37,7 +37,7 @@ export default async function IsNegativeThoughtTrue({
             <h1 className="md:text-heading2-bold text-heading3-bold text-center px-3 mb-6">
               {t("is-thought-true")}
             </h1>
-            <div className="max-w-[400px] sm:mb-0 mb-3">
+            <div className="max-w-[25rem] sm:mb-0 mb-3">
               <Image
                 src="/thoughts/thoughts-step-2.png"
                 alt="step 2: think if that thought is true"
