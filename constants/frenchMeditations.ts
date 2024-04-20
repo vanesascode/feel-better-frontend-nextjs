f0685fb47f765cd4a4d7b95a04b2c7d9a6267098
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-20 22:47:21 +0200

    feat: :sparkles: add French meditations youtube links

---
+++ constants/frenchMeditations.ts
@@ -0,0 +1,68 @@
+export const fiveMinMeditationsFrench = [
+  {
+    id: 1,
+    url: "https://www.youtube.com/embed/3nyQpBu2BSc?si=nxdZ_PoqZ0eER0Q-",
+  },
+  {
+    id: 2,
+    url: "https://www.youtube.com/embed/aKoJ0DfktIw?si=DuY2Ub-fXrLlVsPg",
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/embed/NuGejEt4xp4?si=tJz7GdDQ2XD49FVE",
+  },
+  {
+    id: 4,
+    url: "https://www.youtube.com/embed/nWjVZ0fEjko?si=OOKP_M5GhsSaLFXH",
+  },
+  {
+    id: 5,
+    url: "https://www.youtube.com/embed/qpLqyi0OF0s?si=sV8DSggbr2hC_RGU",
+  },
+];
+
+export const tenMinMeditationsFrench = [
+  {
+    id: 1,
+    url: "https://www.youtube.com/embed/yoi8dFLbYO8?si=H5ZILik2zUCoP4h5",
+  },
+  {
+    id: 2,
+    url: "https://www.youtube.com/embed/vpexQfwROmM?si=fmP5tE4JW2E13dxX",
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/embed/T6VJVRmqVJ8?si=iO-U2j9YnRe71Frd",
+  },
+  {
+    id: 4,
+    url: "https://www.youtube.com/embed/kcX7nzM70oA?si=pDreTL3Rj16wt4kG",
+  },
+  {
+    id: 5,
+    url: "https://www.youtube.com/embed/iPBYAMMRAVM?si=Ct0deIR77frOT5F1",
+  },
+];
+
+export const fifteenMinMeditationsFrench = [
+  {
+    id: 1,
+    url: "https://www.youtube.com/embed/m48el96OSOs?si=RPGNubbg7tQSjt3V",
+  },
+  {
+    id: 2,
+    url: "https://www.youtube.com/embed/Ezv4zsfS-gE?si=hiRtBaj6b3vCwcPH",
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/embed/bad5rdzhNEk?si=gQH1iBpr1K8Otx-D",
+  },
+  {
+    id: 4,
+    url: "https://www.youtube.com/embed/DCrLEqlzW_k?si=eH7UmXfu-v-Ulgjv",
+  },
+  {
+    id: 5,
+    url: "https://www.youtube.com/embed/zJuQWNhSGkA?si=5GAGLrT2Vin-qbfD",
+  },
+];
