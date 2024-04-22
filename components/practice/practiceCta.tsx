46afbe221a431f5fc083a7a0bafdaa229b5e35b2
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-22 21:07:00 +0200

    refactor: :recycle: modify style classes and fix name of practiceCta component

--- components/practice/practiceCta.tsx
+++ components/practice/practiceCta.tsx
@@ -9,7 +9,7 @@ interface PracticeTopCtaProps {
   bold?: boolean;
 }
 
-const PracticeTopCta = ({ children, url, thin, bold }: PracticeTopCtaProps) => {
+const PracticeCta = ({ children, url, thin, bold }: PracticeTopCtaProps) => {
   const { t } = useTranslation();
   const router = useRouter();
 
@@ -17,11 +17,13 @@ const PracticeTopCta = ({ children, url, thin, bold }: PracticeTopCtaProps) => {
     <div className=" flex flex-col justify-start items-center">
       <div className="flex flex-col justify-center items-center max-w-[75.5rem] p-7 md:p-14 gap-5">
         <h2
-          className={`text-white ${
-            thin && "md:text-heading3-thin xs:text-base-thin text-small-thin"
+          className={`${
+            thin &&
+            "md:text-heading3-thin xs:text-body-thin text-base-thin text-gray"
           } ${
-            bold && "md:text-heading3-bold xs:text-base-bold text-small-bold"
-          } text-center px-3`}
+            bold &&
+            "md:text-heading3-bold xs:text-body-bold text-base-bold text-white"
+          } text-center px-3 `}
         >
           {children}
         </h2>
@@ -38,4 +40,4 @@ const PracticeTopCta = ({ children, url, thin, bold }: PracticeTopCtaProps) => {
   );
 };
 
-export default PracticeTopCta;
+export default PracticeCta;
