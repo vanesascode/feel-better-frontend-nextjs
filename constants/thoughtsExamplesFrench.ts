fa0f1c1a5b54001700ba980741fc83c6258c2fc9
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-23 15:47:04 +0200

    feat: :sparkles: add the thoughts examples in French

---
+++ constants/thoughtsExamplesFrench.ts
@@ -0,0 +1,35 @@
+export const thoughtsExamplesFrench = [
+  {
+    negativeThought: "Je n'en peux plus, je me sens submergé par le chagrin.",
+    positiveThought:
+      "Même si cela semble accablant, je peux aller de l'avant et trouver des moyens de gérer ma douleur. Je suis là pour m'aider et trouver des solutions.",
+  },
+  {
+    negativeThought: "Je suis seul là-dedans, personne ne me comprend.",
+    positiveThought:
+      "De nombreuses autres personnes souffrant de douleur chronique luttent également et trouvent des moyens de vivre avec. Je suis ici pour partager des expériences et trouver des solutions ensemble.",
+  },
+  {
+    negativeThought:
+      "Tout cela est de ma faute si je n'ai pas pris soin de moi quand j'étais jeune, je suis stupide.",
+    positiveThought:
+      "Je reconnais que ma santé actuelle peut être influencée par des décisions passées, mais me blâmer ne m'aide pas à améliorer ma situation actuelle. Au lieu de m'en vouloir, je choisis de me concentrer sur les enseignements de mes expériences passées et de prendre des mesures positives pour mieux prendre soin de moi maintenant. Je mérite compassion et soutien sur mon chemin vers la santé et le bien-être",
+  },
+  {
+    negativeThought:
+      "Tout ce que je fais ne fait que me faire encore plus souffrir.",
+    positiveThought:
+      "Il existe de nombreuses façons de gérer la douleur chronique et de trouver des activités qui maident à rester en bonne santé. Je peux explorer différentes options et trouver ce qui fonctionne pour moi.",
+  },
+  {
+    negativeThought: "Cette douleur ne disparaîtra jamais.",
+    positiveThought:
+      "La douleur chronique fait peut-être partie de ma vie, mais elle ne doit pas nécessairement être la seule réalité. Je peux trouver des moyens de gérer cela et de vivre pleinement ma vie, même avec la douleur.",
+  },
+  {
+    negativeThought:
+      "Il y a des moments où je me sens mieux, mais ensuite ça me fait encore mal et j'en ai marre",
+    positiveThought:
+      "J'apprécie les moments où je me sens mieux et j'en suis reconnaissant. Même si la douleur revient, je sais que j'ai ressenti un soulagement dans le passé et que je peux me sentir mieux à nouveau à l'avenir. Je m'efforce de trouver des moyens de gérer la douleur et de prendre soin de moi pendant ces périodes.",
+  },
+];
