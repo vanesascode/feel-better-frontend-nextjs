6b80f7035c744bf8ec917b94e4ca989e8e259dab
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-23 18:42:56 +0200

    refactor: :recycle: add the optional key prop for when I am mapping data into the SelectorButton component

--- components/commons/selectorButton.tsx
+++ components/commons/selectorButton.tsx
@@ -3,11 +3,12 @@ import { ReactNode } from "react";
 interface SelectorButtonProps {
   children: ReactNode;
   onClick: () => void;
+  key?: number;
 }
 
-const SelectorButton = ({ children, onClick }: SelectorButtonProps) => {
+const SelectorButton = ({ children, onClick, key }: SelectorButtonProps) => {
   return (
-    <button onClick={onClick} className="white-green-input">
+    <button onClick={onClick} className="white-green-input" key={key}>
       {children}
     </button>
   );
