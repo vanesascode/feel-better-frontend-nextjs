9bb2d8b9d9b5e9c64d4193ef4dfcffb34d9ec956
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-23 15:47:38 +0200

    feat: :sparkles: add the thoughts examples in Spanish

---
+++ constants/thoughtsExamplesSpanish.ts
@@ -0,0 +1,35 @@
+export const thoughtsExamplesSpanish = [
+  {
+    negativeThought:
+      "No puedo seguir adelante con esto, me siento abrumad@ por el dolor.",
+    positiveThought:
+      "Aunque me sienta abrumador, puedo seguir adelante y encontrar maneras de manejar mi dolor. Estoy aquí para ayudarme y encontrar soluciones.",
+  },
+  {
+    negativeThought: "Estoy sol@ en esto, nadie me entiende.",
+    positiveThought:
+      "Muchos otros con dolor crónico también luchan y encuentran maneras de vivir con él. Estoy aquí para compartir experiencias y encontrar soluciones juntos.",
+  },
+  {
+    negativeThought:
+      "Todo esto es culpa mia por no haberme cuidado cuando era joven, soy estupid@.",
+    positiveThought:
+      "Reconozco que mi salud actual puede estar influenciada por decisiones pasadas, pero culparme a mí mism@ no me ayuda a mejorar mi situación actual. En lugar de castigarme, elijo enfocarme en aprender de mis experiencias pasadas y tomar medidas positivas para cuidar mejor de mí mism@ ahora. Merezco compasión y apoyo en mi camino hacia la salud y el bienestar",
+  },
+  {
+    negativeThought: "Todo lo que hago solo me causa más dolor.",
+    positiveThought:
+      "Hay muchas formas de manejar el dolor crónico y encontrar actividades que me ayuden a mantenerme bien. Puedo explorar diferentes opciones y encontrar lo que funciona para mí.",
+  },
+  {
+    negativeThought: "Este dolor nunca se va a ir.",
+    positiveThought:
+      "El dolor crónico puede ser un parte de mi vida, pero no tiene que ser la única realidad. Puedo encontrar formas de manejarlo y vivir plenamente mi vida, incluso con dolor.",
+  },
+  {
+    negativeThought:
+      "Hay momentos en que me encuentro mejor, pero es que luego me vuelve a doler y estoy hart@",
+    positiveThought:
+      "Aprecio los momentos en los que me siento mejor y estoy agradecida por ellos. Aunque el dolor regrese, sé que he experimentado alivio en el pasado y puedo volver a sentirme mejor en el futuro. Estoy trabajando en encontrar formas de manejar el dolor y cuidar de mí misma en esos momentos",
+  },
+];
