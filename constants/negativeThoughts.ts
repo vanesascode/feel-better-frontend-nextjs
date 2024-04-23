d5ead078f155a85f23924571fbbb057609d8342c
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-23 18:42:10 +0200

    feat: :sparkles: add the negative thoughts of the choose-negative-thought page into a constants file

---
+++ constants/negativeThoughts.ts
@@ -0,0 +1,161 @@
+export const englishNegativeThoughts = [
+  {
+    id: 1,
+    negativeThought: "Nobody understands what I'm going through, I feel alone.",
+  },
+  {
+    id: 2,
+    negativeThought: "I will never be able to do the things I loved again.",
+  },
+  {
+    id: 3,
+    negativeThought: "My pain will never get better no matter what I do.",
+  },
+  {
+    id: 4,
+    negativeThought: "I feel like I am a burden to my loved ones.",
+  },
+  {
+    id: 5,
+    negativeThought:
+      "I can't go through with this, I feel overwhelmed with grief.",
+  },
+  {
+    id: 6,
+    negativeThought: "I'm alone in this, no one understands me.",
+  },
+  {
+    id: 7,
+    negativeThought: "I'm not strong enough to get through this.",
+  },
+  {
+    id: 8,
+    negativeThought: "I have no control over my own body.",
+  },
+  {
+    id: 9,
+    negativeThought: "I should be able to handle the pain better.",
+  },
+  {
+    id: 10,
+    negativeThought: "I can't plan anything because my pain is unpredictable.",
+  },
+  {
+    id: 11,
+    negativeThought: "My pain will only get worse over time.",
+  },
+  {
+    id: 12,
+    negativeThought:
+      "I am ashamed that others think I am exaggerating the pain.",
+  },
+];
+
+export const spanishNegativeThoughts = [
+  {
+    id: 1,
+    negativeThought: "Nadie entiende que me estoy pasando, me siento sol@.",
+  },
+  {
+    id: 2,
+    negativeThought: "Nunca puedo hacer las cosas que amo.",
+  },
+  {
+    id: 3,
+    negativeThought: "Mi dolor nunca mejorará, no importa lo que haga.",
+  },
+  {
+    id: 4,
+    negativeThought: "No tengo control sobre mi propio cuerpo.",
+  },
+  {
+    id: 5,
+    negativeThought: "Siento que soy una carga para mis seres queridos.",
+  },
+  {
+    id: 6,
+    negativeThought: "No soy lo suficientemente fuerte para superar esto.",
+  },
+  {
+    id: 7,
+    negativeThought:
+      "No puedo planificar nada porque mi dolor es impredecible.",
+  },
+  {
+    id: 8,
+    negativeThought: "Mi dolor solo hará que empeorar con el tiempo.",
+  },
+  {
+    id: 9,
+    negativeThought: "Siento que debería manejar mejor mi dolor.",
+  },
+  {
+    id: 10,
+    negativeThought:
+      "Mi vida es un total desastre si no puedo manejar mi dolor.",
+  },
+  {
+    id: 11,
+    negativeThought:
+      "No se si quiero seguir adelante con esto, me siento abrumad@ por el dolor.",
+  },
+  {
+    id: 12,
+    negativeThought:
+      "Me avergüenza que otros piensen que estoy exagerando el dolor.",
+  },
+];
+
+export const frenchNegativeThoughts = [
+  {
+    id: 1,
+    negativeThought: "Personne ne comprend ce que je vis, je me sens seul.",
+  },
+  {
+    id: 2,
+    negativeThought: "Je ne pourrai plus jamais faire les choses que j'aimais.",
+  },
+  {
+    id: 3,
+    negativeThought: "Ma douleur ne s'améliorera jamais, quoi que je fasse.",
+  },
+  {
+    id: 4,
+    negativeThought: "J'ai l'impression d'être un fardeau pour mes proches.",
+  },
+  {
+    id: 5,
+    negativeThought: "Je ne suis pas assez fort pour m'en sortir.",
+  },
+  {
+    id: 6,
+    negativeThought: "Je n'ai aucun contrôle sur mon propre corps.",
+  },
+  {
+    id: 7,
+    negativeThought:
+      "Je ne peux rien planifier car ma douleur est imprévisible.",
+  },
+  {
+    id: 8,
+    negativeThought: "Ma douleur ne fera qu'empirer avec le temps.",
+  },
+  {
+    id: 9,
+    negativeThought: "Je devrais être capable de mieux gérer la douleur.",
+  },
+  {
+    id: 10,
+    negativeThought: "Je ne peux pas planifier ce qui se passe.",
+  },
+  {
+    id: 11,
+    negativeThought:
+      "Ma vie est un total désastre si je ne peux pas gérer ma douleur.",
+  },
+  {
+    id: 12,
+    negativeThought:
+      "J'ai honte que les autres pensent que j'exagère la douleur.",
+  },
+];
