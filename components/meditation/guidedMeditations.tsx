5508e65f8f8ff6b25fdb5cb1997bc8c7e2f2f2ae
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-23 17:43:56 +0200

    refactor: :recycle: change the position of the ScrollToTop component in the GuidedMeditations one so it properly words

--- components/meditation/guidedMeditations.tsx
+++ components/meditation/guidedMeditations.tsx
@@ -6,8 +6,8 @@ import MeditationBars from "./meditationBars";
 const GuidedMeditations = () => {
   return (
     <>
-      <ScrollToTop />
       <div className="bg-gray-gradient flex flex-col justify-center items-center px-5 pt-5 sm1:px-10 sm1:pt-10 pb-20 relative gap-10">
+        <ScrollToTop />
         <MeditationBars />
       </div>
     </>
