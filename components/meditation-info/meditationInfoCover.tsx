6cffcf9ad2101d5593dc24816bec9d4863cd484c
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-26 16:31:34 +0200

    refactor: :recycle: format the code in the MeditationInfoCover component

--- components/meditation-info/meditationInfoCover.tsx
+++ components/meditation-info/meditationInfoCover.tsx
@@ -15,10 +15,7 @@ const MeditationInfoCover = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <h1
-          className="absolute xl:text-cover1-semibold lg:text-cover2-semibold sm:text-cover3-semibold text-cover4-semibold text-white px-3 xxs:px-4 sm:px-6 md:px-10 md1:px-20 lg:px-16 xxl:px-32 xxxl:px-44 xxxxl:px-64
-     top-[57%] xs:top-[60%] sm:top-[55%] sm1:top-[55%] sm2:top-[20%] lg:top-[30%] xl:top-[20%]  "
-        >
+        <h1 className="absolute xl:text-cover1-semibold lg:text-cover2-semibold sm:text-cover3-semibold text-cover4-semibold text-white px-3 xxs:px-4 sm:px-6 md:px-10 md1:px-20 lg:px-16 xxl:px-32 xxxl:px-44 xxxxl:px-64 top-[57%] xs:top-[60%] sm:top-[55%] sm1:top-[55%] sm2:top-[20%] lg:top-[30%] xl:top-[20%]  ">
           {t("do")} <br />
           {t("guided")} <br />
           {t("meditation")}
