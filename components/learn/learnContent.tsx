39eb5ef2348ec48b54054e090b1bef751b6bd844
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-26 21:00:51 +0200

    feat: :sparkles: add the learnContent component

---
+++ components/learn/learnContent.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useTranslation } from "react-i18next";
+import LearnCover from "./learnCover";
+import LearnIntro from "./learnIntro";
+import QuestionDropdown from "../commons/questionDropdown";
+import QuestionOneContent from "./questionOneContent";
+import QuestionTwoContent from "./questionTwoContent";
+import QuestionThreeContent from "./questionThreeContent";
+import QuestionFourContent from "./questionFourContent";
+import QuestionFiveContent from "./questionFiveContent";
+
+const LearnContent = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <LearnCover />
+      <LearnIntro />
+      <QuestionDropdown
+        className="bg-dark-green-2 contentTitleBar"
+        copy={t("question-1")}
+      >
+        <QuestionOneContent />
+      </QuestionDropdown>
+      <QuestionDropdown
+        className="bg-dark-green-1 contentTitleBar"
+        copy={t("question-2")}
+      >
+        <QuestionTwoContent />
+      </QuestionDropdown>
+      <QuestionDropdown
+        className="bg-cta-green contentTitleBar"
+        copy={t("question-3")}
+      >
+        <QuestionThreeContent />
+      </QuestionDropdown>
+      <QuestionDropdown
+        className="bg-dark-green-2 contentTitleBar"
+        copy={t("question-4")}
+      >
+        <QuestionFourContent />
+      </QuestionDropdown>
+      <QuestionDropdown
+        className="bg-dark-green-1 contentTitleBar"
+        copy={t("question-5")}
+      >
+        <QuestionFiveContent />
+      </QuestionDropdown>
+      {t("bibliography")}
+    </>
+  );
+};
+
+export default LearnContent;
