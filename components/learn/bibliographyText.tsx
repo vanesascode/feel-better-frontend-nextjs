03cf2fed22f2e7d7e712648b88f51881dccaa9ae
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-27 15:04:03 +0200

    feat: :sparkles: add the BibliographyText component

---
+++ components/learn/bibliographyText.tsx
@@ -0,0 +1,138 @@
+const BibliographyText = () => {
+  return (
+    <div className="p-horizontal text-gray text-base-thin md:text-body-thin flex flex-col gap-5">
+      <p>
+        Almadi, T., Cathers, I., Chow, C.M. (2013). Associations among
+        work-related stress, cortisol, inflammation, and metabolic syndrome.
+        Psychophysiology, 50 (9), 821-830.
+      </p>
+      <p>
+        Arntz, A. y Claassens, L. (2004). The meaning of pain influences its
+        experienced intensity. Pain, 109, 20-25.
+      </p>
+      <p>
+        Asmundson, G.J.G., Parkerson, H.A., Petter, M. y Noel, M. (2012). What
+        is the role of fear and escape/avoidance in chronic pain? Models,
+        structural analysis and future directions. Pain Management, 2, 295-303.
+      </p>
+      <p>
+        Desbordes G, Negi LT, Pace TWW, Wallace BA, Raison CL, Schwartz EL.
+        (2012). Effects of mindful-attention and compassion meditation training
+        on amygdala response to emotional stimuli in an ordinary, non-meditative
+        state. Front Hum Neurosci, 6: 292.
+      </p>
+      <p>
+        Edwards, RR, Dworkin RH, Sullivan MD, Turk DC, Wasan AD (2016). The Role
+        of Psychosocial Processes in the Development and Maintenance of Chronic
+        Pain. Journal of Pain, 17 T70-T92.
+      </p>
+      <p>
+        Evanger LN, Flo-Groeneboom E, Sørensen L, Schanche E. (2024).
+        Mindfulness-based cognitive therapy improves insomnia symptoms in
+        individuals with recurrent depression: secondary analyses from a
+        randomized controlled trial. Front Psychiatry, 14:1231040.
+      </p>
+      <p>
+        Goldin PR, Gross JJ. (2010). Effects of mindfulness-based stress
+        reduction (MBSR) on emotion regulation in social anxiety disorder.
+        Emotion, 10: 83-91.
+      </p>
+      <p>
+        Greenwald JD, Shafritz KM. (2018).An integrative neuroscience framework
+        for the treatment of chronic pain: from cellular alterations to
+        behavior. Front Integr Neurosci, 12: 18.
+      </p>
+      <p>
+        Hölzel BK, Hoge EA, Greve DN, Gard T, Creswell JD, Brown KW, et al.
+        (2013).Neural mechanisms of symptom improvements in generalized anxiety
+        disorder following mindfulness training. Neuroimage, 2: 448-58.
+      </p>
+      <p>
+        Jain S, Shapiro SL, Swanick S, Roesch SC, Mills PJ, Bell I, et al.
+        (2007). A randomized controlled trial of mindfulness meditation versus
+        relaxation training: effects on distress, positive states of mind,
+        rumination, and distraction. Ann Behav Med, 33: 11-21.
+      </p>
+      <p>
+        Keefe, F. J., & Sharpe, L. (2019). Graded exposure therapy for chronic
+        pain: A practical guide for clinicians. Journal of Clinical Psychology,
+        75(2), 232-247.
+      </p>
+      <p>
+        Lazar SW, Kerr CE, Wasserman RH, Gray JR, Greve DN, Treadway MT, et al.
+        (2005). Meditation experience is associated with increased cortical
+        thickness. Neuroreport, 16: 1893-7.
+      </p>
+      <p>
+        Lovell, B., Moss, M., Wetherell, M.A. (2011). Perceived stress, common
+        health complaints and diurnal patterns of cortisol secretion in young,
+        otherwise healthy individuals. Horm. Behav. 60 (3), 301-305.
+      </p>
+      <p>
+        Ludwig DS, Kabat Zinn J. (2008). Mindfulness in medicine. JAMA,
+        300(11):1350\2.
+      </p>
+      <p>
+        Mackey, Sean C. (2013). Central Neuroimaging of Pain. Journal of Pain,
+        14 328-331.
+      </p>
+      <p>
+        Michael W Stroud, Beverly E Thorn, Mark P Jensen, Jennifer L Boothby,
+        (2000). The relation between pain beliefs, negative thoughts, and
+        psychosocial functioning in chronic pain patients. PAIN, Volume 84,
+        Issues 2-3, Pages 347-352
+      </p>
+      <p>
+        Ramírez-Maestre, C., Esteve, R., López, A.E. (2012). The role of
+        optimism and pessimism in chronic pain patients adjustment. The Spanish
+        Journal of Psychology, 15, 286-294.
+      </p>
+      <p>
+        Robins CJ, Keng SL, Ekblad AG, Brantley JG. (2012). Effects of
+        mindfulness-based stress reduction on emotional experience and
+        expression: a randomized controlled trial. J Clin Psychol, 68: 117-31.
+      </p>
+      <p>
+        Rusch HL, Rosario M, Levison LM, Olivera A, Livingston WS, Wu T, et al.
+        (2019). The effect of mindfulness meditation on sleep quality: a
+        systematic review and meta-analysis of randomized controlled trials. Ann
+        N Y Acad Sci,1445:5-16.
+      </p>
+      <p>
+        Seminowicz, D.A., Shpaner, M., Keaser, M.L., Krauthamer, G.M., Mantegna,
+        J., Dumas, J.A., Newhouse, P.A., Filippi, C.G., Keefe, F.J., and Naylor,
+        M.R. (2013). Cognitive-behavioral therapy increases prefrontal cortex
+        gray matter in patients with chronic pain. Journal of Pain, 14
+        1573-1584.
+      </p>
+      <p>
+        Seminowicz, D.A., Wideman, T.H., Naso, L., Hatami-Khoroushahi, Z.,
+        Fallatah, S., Ware, M.A., Jarzem, P., Bushnell, M.C., Shir, Y., Ouellet,
+        J.A., and Stone, L.S. (2011). Effective treatment of chronic low back
+        pain in humans reverses abnormal brain anatomy and function. Journal of
+        Neuroscience, 31 7540-7550.
+      </p>
+      <p>
+        Sheinberg, R, Campbell, C, Kearson, A, Burton, E, Letzen, J (2019).
+        Childhood Adversity Linked to Heightened Pain Sensitivity in Adults,
+        Journal of Pain, 20 S4-S5.
+      </p>
+      <p>
+        Tang YY, Lu Q, Geng X, Stein EA, Yang Y, Posner MI. (2010).Short-term
+        meditation induces white matter changes in the anterior cingulate. Proc
+        Natl Acad Sciences U S A, 107: 15649-52.
+      </p>
+      <p>
+        Turk, D. C., & Barlow, D. H. (2018). Fear of pain and its role in
+        chronic pain. Pain, 159(S1), S1-S7.
+      </p>
+      <p>
+        Van Hecke, O., Torrance, N., & Eccleston, C. (2019). Graded exposure
+        therapy for chronic pain: A systematic review and meta-analysis of
+        randomized controlled trials. Journal of Rheumatology, 46(4), 444-455.
+      </p>
+    </div>
+  );
+};
+
+export default BibliographyText;
