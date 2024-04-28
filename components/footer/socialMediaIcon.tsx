a4401532d0fb3cade7a52fbf86b43c8dc82e06bc
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-28 09:00:18 +0200

    refactor: :recycle: remove the key prop from the socialMediaIcon component since it cannot be a prop in this case

--- components/footer/socialMediaIcon.tsx
+++ components/footer/socialMediaIcon.tsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
 
 interface SocialMediaIconProps {
-  key: number;
   href: string;
   src: string;
   alt: string;
 }
 
-const socialMediaIcon = ({ key, href, src, alt }: SocialMediaIconProps) => {
+const socialMediaIcon = ({ href, src, alt }: SocialMediaIconProps) => {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noreferrer"
-      className="cursor-pointer"
-      key={key}
-    >
+    <a href={href} target="_blank" rel="noreferrer" className="cursor-pointer">
       <Image src={src} alt={alt} width={100} height={100} />
     </a>
   );
