5bac6f27938416678c5389ef80d9cb09251a19a9
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-30 17:24:51 +0200

    refactor: :recycle: modify the links of the the social media platforms related to FeelBetter

--- constants/socialMediaIcons.ts
+++ constants/socialMediaIcons.ts
@@ -1,25 +1,25 @@
 export const socialMediaIcons = [
   {
     id: 1,
-    href: "https://www.facebook.com",
+    href: "https://www.facebook.com/profile.php?id=61559103510332",
     src: "/footer/facebook.svg",
     alt: "facebook icon",
   },
   {
     id: 2,
-    href: "https://www.instagram.com",
+    href: "https://www.instagram.com/confeelbetter",
     src: "/footer/instagram.svg",
     alt: "instagram icon",
   },
   {
     id: 3,
-    href: "https://www.twitter.com",
+    href: "https://twitter.com/ConFeelBetter",
     src: "/footer/x.svg",
     alt: "x twitter icon",
   },
   {
     id: 4,
-    href: "https://www.linkedin.com",
+    href: "https://www.linkedin.com/company/mejoraconfeelbetter/?viewAsMember=true",
     src: "/footer/linkedin.svg",
     alt: "linkedin icon",
   },
