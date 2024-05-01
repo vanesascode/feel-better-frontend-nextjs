5c2018c08b5f2a63e8939f49dc502cdab8befd85
vanesa <vanesa.juarez.paris@gmail.com>
2024-05-01 23:06:16 +0200

    refactor: :recycle: rearrange correctly the paragraphs in the QuestionFiveContent component

--- components/learn/questionFiveContent.tsx
+++ components/learn/questionFiveContent.tsx
@@ -14,9 +14,7 @@ const QuestionFiveContent = () => {
             t("question-5-explanation-1") +
             t("question-5-explanation-5") +
             t("question-5-explanation-3") +
-            t("question-5-explanation-4") +
-            t("question-5-explanation-5") +
-            t("question-5-explanation-6")
+            t("question-5-explanation-4")
           }
         />
         <section>
@@ -26,9 +24,6 @@ const QuestionFiveContent = () => {
               <br />
               <br />
               {t("question-5-explanation-2")}
-              <br />
-              <br />
-              {t("question-5-explanation-3")}
             </p>
             <div className="max-sm2:hidden block">
               <ImageComponent
@@ -43,13 +38,10 @@ const QuestionFiveContent = () => {
               />
             </div>
             <p className="md:text-body-thin text-base-thin text-gray lg:mb-5 pt-5">
+              {t("question-5-explanation-3")}
+              <br />
+              <br />
               {t("question-5-explanation-4")}
-              <br />
-              <br />
-              {t("question-5-explanation-5")}
-              <br />
-              <br />
-              {t("question-5-explanation-6")}
             </p>
           </article>
         </section>
